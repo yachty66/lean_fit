a86c5a151e3708bf6a06402b3d3998082422fdb5
yachty66 <maxhager28@gmail.com>
2024-01-08 01:58:32 +0100

    init

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-"""
-1. run script every day 23:50 
-2. if there is an action on that day than take that one and add it to duration & pace and also the link from that run
-3. add it to the cell of the respective data 
-
-"""
 import requests
 from dotenv import load_dotenv
 from datetime import date, datetime
 import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv()  
 access_token = os.getenv('access_token')
 
 def main():
@@ -17,16 +13,13 @@ def main():
     main function
     """
     activities=strava()
-    #print("activities")
-    #print(activities)
     date=get_date()
     daily_action=get_daily_action_from_strava(activities, date)
-    print(daily_action)
-    # for activity in activities:
-    #     #print(activity)
-    #     print(datetime.strptime(activity['start_date'], '%Y-%m-%dT%H:%M:%SZ').strftime('%Y-%m-%d'))
-    #     break
-    #     #print(activity['start_date_local'])
+    duration=get_duration_from_strava(daily_action)
+    duration_in_minutes=convert_to_minutes(duration)
+    speed = get_pace_from_strava(daily_action)
+    link = get_link_from_strava(daily_action)
+    write_duration_and_pace_to_sheet(date, duration_in_minutes, speed, link)
 
 def strava():
     """
@@ -46,7 +39,6 @@ def get_daily_action_from_strava(activities, date):
     """
     gets the daily action from strava
     """
-    # date = "2023-10-04"
     daily_activity = None
     for activity in activities:
         if datetime.strptime(activity['start_date'], '%Y-%m-%dT%H:%M:%SZ').strftime('%Y-%m-%d') == date:
@@ -54,46 +46,54 @@ def get_daily_action_from_strava(activities, date):
             break
     return daily_activity
 
-def get_daily_action():
-    """
-    gets the daily action from the google sheet
-    """
-    pass
-
-def get_duration():
+def get_duration_from_strava(daily_activity):
     """
     gets the duration from strava
     """
-    pass
+    return daily_activity['moving_time']
 
-def get_pace():
+def convert_to_minutes(duration):
     """
-    gets the pace from strava
+    converts the duration to minutes
     """
-    pass
+    minutes = duration // 60
+    seconds = duration % 60
+    return f"{minutes}.{seconds}"
 
-def write_duration_to_sheet():
+def get_pace_from_strava(daily_activity):
+    """
+    gets the pace from strava and converts it to minutes per kilometer
+    """
+    # Strava's average_speed is in meters per second
+    average_speed_mps = daily_activity['average_speed']
+    # Convert speed to pace (minutes per kilometer)
+    if average_speed_mps > 0:  # To avoid division by zero
+        pace_seconds_per_km = 1000 / average_speed_mps  # seconds per kilometer
+        pace_minutes_per_km = pace_seconds_per_km / 60  # minutes per kilometer
+        # Format the pace into minutes and seconds per kilometer
+        pace_minutes = int(pace_minutes_per_km)
+        pace_seconds = int((pace_minutes_per_km - pace_minutes) * 60)
+        return f"{pace_minutes}:{pace_seconds:02d}"
+    
+def get_link_from_strava(daily_activity):
+    """
+    gets the link from strava
+    """
+    return daily_activity['external_id']
+
+def write_duration_and_pace_to_sheet(date, duration, pace):
     """
     writes the duration data to the google sheet
     """
-    pass
+    date_obj = datetime.strptime(date, '%Y-%m-%d')
+    formatted_date = date_obj.strftime('%d/%m/%Y')
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('key.json', scope)
+    client = gspread.authorize(creds)
+    sheet = client.open('lean_fit').sheet1
+    first_row_values = sheet.col_values(1)
+    column_index = first_row_values.index(formatted_date) + 1 
+    sheet.update_cell(column_index, 5, duration)
+    sheet.update_cell(column_index, 6, pace)
 
-def write_pace_to_sheet():
-    """
-    writes the pace data to the google sheet
-    """
-    pass
-
-def write_link_to_sheet():
-    """
-    writes the link to the run to the google sheet
-    """
-    pass
-
-
-if __name__ == "__main__":
-    main()
-
-#https://www.strava.com/oauth/authorize?client_id=119488&redirect_uri=http://localhost:8000&response_type=code&scope=activity:read_all
-
-#http://localhost:8000/?state=&code=65cff7e81cc8ace8deb4832cb3f7a664d75efefe&scope=read,activity:write
